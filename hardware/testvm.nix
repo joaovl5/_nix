74d1fbb0f6859c5a9ff940d9c07e6f21891ed4d5
João Pedro <vieiraleao2005@gmail.com>
2025-12-19 07:28:44 -0300

    .

--- hardware/testvm.nix
+++ hardware/testvm.nix
@@ -15,29 +15,27 @@
 
   networking.useDHCP = lib.mkDefault true;
   networking.usePredictableInterfaceNames = true;
-  config = {
-    interface.hardware.networking = true;
-    interface.hardware.gui = true;
+  interface.hardware.networking = true;
+  interface.hardware.gui = true;
 
-    boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sr_mod" "virtio_blk" ];
-    boot.initrd.kernelModules = [ ];
-    boot.kernelModules = [ "kvm-amd" ];
-    boot.extraModulePackages = [ ];
-    boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sr_mod" "virtio_blk" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
-    fileSystems."/" =
-      {
-        device = "UUID=90ecc4f4-65ca-47df-b362-5184cda952d0";
-        fsType = "bcachefs";
-      };
+  fileSystems."/" =
+    {
+      device = "UUID=90ecc4f4-65ca-47df-b362-5184cda952d0";
+      fsType = "bcachefs";
+    };
 
-    swapDevices =
-      [{ device = "/dev/disk/by-uuid/14520cb5-f22d-4354-83b3-44c10f2e0574"; }];
+  swapDevices =
+    [{ device = "/dev/disk/by-uuid/14520cb5-f22d-4354-83b3-44c10f2e0574"; }];
 
-    environment.defaultPackages = with pkgs; [
-      openrgb
-    ];
-  };
+  environment.defaultPackages = with pkgs; [
+    openrgb
+  ];
 
   # rgb
   hardware.i2c = {
