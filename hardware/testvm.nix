07e94b5b45607ba906abdc45037a5cc017af0525
João Pedro <vieiraleao2005@gmail.com>
2025-12-20 08:20:43 -0300

    .

--- hardware/testvm.nix
+++ hardware/testvm.nix
@@ -8,7 +8,7 @@
   imports = [
     ./modules/pipewire.nix
     ./modules/grub.nix
-    ./disko/test_bcachefs.nix
+    ./disko/testvm_btrfs.nix
     (modulesPath + "/profiles/qemu-guest.nix")
     inputs.nixpkgs.nixosModules.notDetected
   ];
