2002c7fc702af475fc31ee2ae4bbf53635c736e6
João Pedro <vieiraleao2005@gmail.com>
2025-12-28 16:55:07 -0300

    .

--- users/services/traefik/default.nix
+++ users/services/traefik/default.nix
@@ -24,9 +24,9 @@ in {
           service = "technitium_dns";
           entryPoints = ["web"];
         };
-        services.technitium_dns = {
-          loadBalancer.servers = ["http://${host_ip}:${builtins.toString http_port}"];
-        };
+        # services.technitium_dns = {
+        #   loadBalancer.servers = ["http://${host_ip}:${builtins.toString http_port}"];
+        # };
       }))
     ];
   };
