a18821276a0aec9c88be6e058021b60cc4f23479
João Pedro <vieiraleao2005@gmail.com>
2026-01-02 17:56:36 -0300

    BAGA

--- users/modules/gnome.nix
+++ users/modules/gnome.nix
@@ -4,5 +4,6 @@
   services.gnome.core-apps.enable = false;
   services.gnome.core-developer-tools.enable = false;
   services.gnome.games.enable = false;
+  services.gnome.gcr-ssh-agent.enable = false;
   environment.gnome.excludePackages = with pkgs; [gnome-tour gnome-user-docs];
 }
