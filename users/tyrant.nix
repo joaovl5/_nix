f52db19c914f2c62459a3e9752263f4c6f763d96
João Pedro <vieiraleao2005+lavpc@gmail.com>
2026-01-10 07:52:21 -0300

    .

--- users/tyrant.nix
+++ users/tyrant.nix
@@ -27,7 +27,7 @@ in {
   users.users.${cfg.username} = {
     isNormalUser = true;
     extraGroups = ["wheel" "libvirt"];
-    hashedPasswordFile = config.sops.secrets.password_hash_server.path;
+    hashedPasswordFile = config.sops.secrets.password_hash.path;
     shell = pkgs.fish;
   };
 
