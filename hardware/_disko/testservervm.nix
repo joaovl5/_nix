6920a3303225d7441332f0862605b8da230869cf
Joao Pedro <vieiraleao2005@gmail.com>
2026-02-11 12:20:10 -0300

    .

--- hardware/_disko/testservervm.nix
+++ hardware/_disko/testservervm.nix
@@ -12,11 +12,30 @@ in {
       type = "gpt";
       partitions = {
         boot = mbr {};
-        luks_p1 = luks {name = "p1";} {
-          type = "btrfs";
-          subvolumes = {
-            "@root" = subvolume {mp = "/";};
+        root = {
+          size = "100%";
+          content = {
+            type = "lvm_pv";
+            vg = "main_pool";
           };
+        };
+        # luks_p1 = luks {name = "p1";} {
+        #   type = "btrfs";
+        #   subvolumes = {
+        #     "@root" = subvolume {mp = "/";};
+        #   };
+        # };
+      };
+    };
+  };
+  disko.devices.lvm_vg.main_pool = {
+    type = "lvm_vg";
+    lvs.system = {
+      size = "100%";
+      content = luks {name = "p1";} {
+        type = "btrfs";
+        subvolumes = {
+          "@root" = subvolume {mp = "/";};
         };
       };
     };
