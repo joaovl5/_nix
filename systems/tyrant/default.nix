2aa41d81aaa84c73b4a6a5d4ed30eb6bc682c184
Joao Pedro <vieiraleao2005@gmail.com>
2026-03-04 08:47:01 -0300

    feat: wireguard tunnels setup
    - tunnels for relaying incoming traffic from temperance selectively into
    intranet
    - tunnels for passing selected traffic into temperance as a vpn
    
    Signed-off-by: Joao Pedro <vieiraleao2005@gmail.com>

--- systems/tyrant/default.nix
+++ systems/tyrant/default.nix
@@ -1,42 +1,12 @@
-_: {
+{mylib, ...}: {
   imports = [
     ../_bootstrap/server.nix
+    (mylib.hosts.host_config "tyrant")
   ];
 
   my = {
     server.enable = true;
     host.title_file = ./assets/title.txt;
     host.password.sops_key = "server";
-
-    "unit.octodns" = {
-      enable = true;
-    };
-
-    "unit.traefik" = {
-      enable = true;
-    };
-
-    "unit.pihole" = {
-      enable = true;
-      dns = {
-        interface = "enp3s0f1";
-      };
-    };
-
-    "unit.litellm" = {
-      enable = true;
-    };
-
-    "unit.nixarr" = {
-      enable = true;
-    };
-
-    "unit.soularr" = {
-      enable = true;
-    };
-
-    "unit.fxsync" = {
-      enable = true;
-    };
   };
 }
