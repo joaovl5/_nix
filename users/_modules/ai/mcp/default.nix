dd9e92e5587d1bcda0982e28367cfbb6600a3094
Joao Pedro <vieiraleao2005@gmail.com>
2026-03-21 18:03:03 -0300

    fix(ai): reword sys. prompt and remove memory mcp
    Signed-off-by: Joao Pedro <vieiraleao2005@gmail.com>

--- users/_modules/ai/mcp/default.nix
+++ users/_modules/ai/mcp/default.nix
@@ -9,20 +9,16 @@
       command = "uvx";
       args = [pkg];
     };
-    npx = pkg: {
-      command = "npx";
-      args = ["-y" pkg];
-    };
+    # npx = pkg: {
+    #   command = "npx";
+    #   args = ["-y" pkg];
+    # };
   in {
     programs.mcp = {
       enable = true;
       servers = {
         # nixos docs, etc
         nixos.command = get "mcp-nixos";
-        # # repository understanding/compression for LLMs
-        # repomix.command = get "repomix";
-        # llm memory
-        memory = npx "@modelcontextprotocol/server-memory";
         # fetch pages
         fetch = uvx "mcp-server-fetch";
         # search
