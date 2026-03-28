2510c6772205ce489e5721ca4be84c2a486fe843
Joao Pedro <vieiraleao2005@gmail.com>
2026-03-28 12:27:49 -0300

    fix(ai): undo unnecessary merge prompts
    Signed-off-by: Joao Pedro <vieiraleao2005@gmail.com>

--- users/_modules/ai/_prompts/default.nix
+++ users/_modules/ai/_prompts/default.nix
@@ -1,22 +1,8 @@
 {
-  hm = {
-    pkgs,
-    # inputs,
-    ...
-  }: let
-    merged_skills = pkgs.symlinkJoin {
-      name = "opencode-skills";
-      paths = [
-        ./skills
-        # i don't need those prompts
-        # "${inputs.anthropic-skills}/skills"
-      ];
-    };
-  in {
+  hm = _: {
     hybrid-links.links.agents_skills = {
-      from = merged_skills;
+      from = ./skills;
       to = "~/.agents/skills";
-      hybrid = false;
     };
   };
 }
