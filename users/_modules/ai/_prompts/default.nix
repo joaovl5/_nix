89664accb5f2752b736340431b331958cafe123c
Joao Pedro <vieiraleao2005@gmail.com>
2026-03-30 08:46:17 -0300

    fix(ai): add alternative skills path
    Signed-off-by: Joao Pedro <vieiraleao2005@gmail.com>

--- users/_modules/ai/_prompts/default.nix
+++ users/_modules/ai/_prompts/default.nix
@@ -1,8 +1,15 @@
 {
   hm = _: {
-    hybrid-links.links.agents_skills = {
-      from = ./skills;
-      to = "~/.agents/skills";
+    hybrid-links.links = {
+      agents_skills = {
+        from = ./skills;
+        to = "~/.agents/skills";
+      };
+      # some use this:
+      agents_skills_2 = {
+        from = ./skills;
+        to = "~/.config/agents/skills";
+      };
     };
   };
 }
