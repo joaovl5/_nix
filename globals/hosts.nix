1bf1b7ca1e940421b1432ef4b6a691684c2df2be
Joao Pedro <vieiraleao2005@gmail.com>
2026-04-04 15:17:40 -0300

    refactor: move tmpfiles to systems
    Signed-off-by: Joao Pedro <vieiraleao2005@gmail.com>

--- globals/hosts.nix
+++ globals/hosts.nix
@@ -139,13 +139,6 @@ rec {
           };
         };
       };
-
-      systemd.tmpfiles.rules = [
-        "d /var/lib/backups 0755 root root -"
-        "d /var/lib/backups/repos 0755 root root -"
-        "d /var/lib/backups/repos/tyrant 0700 root root -"
-        "d /var/lib/backups/repos/lavpc 0750 tyrant tyrant -"
-      ];
     };
   };
   temperance = let
--- systems/tyrant/default.nix
+++ systems/tyrant/default.nix
@@ -9,4 +9,11 @@
     host.title_file = ./assets/title.txt;
     host.password.sops_key = "server";
   };
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/backups 0755 root root -"
+    "d /var/lib/backups/repos 0755 root root -"
+    "d /var/lib/backups/repos/tyrant 0700 root root -"
+    "d /var/lib/backups/repos/lavpc 0750 tyrant tyrant -"
+  ];
 }
