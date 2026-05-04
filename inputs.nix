c66bf224f5feb59b14a0b02af4a9cdef95ff655b
Joao Pedro <vieiraleao2005@gmail.com>
2026-05-04 13:29:03 -0300

    chore(npins): drop unused niri source metadata

--- inputs.nix
+++ inputs.nix
@@ -1,23 +1,5 @@
 let
-  source_metadata = {
-    niri-stable = {
-      lastModified = 1756556321;
-      lastModifiedDate = "20250830121841";
-    };
-    niri-unstable = {
-      lastModified = 1777627080;
-      lastModifiedDate = "20260501091800";
-    };
-    xwayland-satellite-stable = {
-      lastModified = 1755491097;
-      lastModifiedDate = "20250818042457";
-    };
-    xwayland-satellite-unstable = {
-      lastModified = 1773622265;
-      lastModifiedDate = "20260316005105";
-    };
-  };
-  normalize_source = name: source:
+  normalize_source = _name: source:
     source
     // (
       if source ? revision
@@ -26,8 +8,7 @@ let
         shortRev = builtins.substring 0 7 source.revision;
       }
       else {}
-    )
-    // (source_metadata.${name} or {});
+    );
   sources = builtins.mapAttrs normalize_source (import ./npins);
   input_overrides = import ./input-overrides.nix;
 in
