8c93913a4f2ae10aa5be878d19a979302c39f660
Joao Pedro <vieiraleao2005@gmail.com>
2026-08-06 13:32:53 -0300

    remove garnix
    Signed-off-by: Joao Pedro <vieiraleao2005@gmail.com>

--- modules/aspects/base/nix.nix
+++ modules/aspects/base/nix.nix
@@ -39,14 +39,12 @@ _: {
                 substituters = [
                   "https://nix-community.cachix.org"
                   "https://cache.nixos.org/"
-                  "https://cache.garnix.io"
                   "https://hyprland.cachix.org"
                   "https://cache.numtide.com"
                 ];
                 trusted-public-keys = [
                   "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
                   "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-                  "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
                   "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
                   "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
                 ];
